57a02f065942f5df25e71fd0da2cd3a3c7dc3160
Dernise <dernise@live.fr>
2014-03-21 13:27:40 +0100

    Hardcodded map

--- game.js
+++ game.js
@@ -3,19 +3,74 @@ $(document).ready(function() {
   window.socketClient = new Network("realmd.warleague.fr", 8080);
   //initialise UI
   window.uiManager = new WarleagueUI();
-  window.uiManager.initUI();
+  window.game = new WarleagueGame();
 });
 
 /*
  * On resize, correct de height of the chat and width of the navbar
  */
 $( window ).resize(function() { //Resize chat
-    var chat_height = $( window ).height() - 55;
-    var chat_left = $( window ).width() - 342;
-    var menu_width = $( window ).width();
-    var chat_messages_height = $(window).height() - 115;
-    $("#chat").css("left", chat_left+ "px");
-    $("#chat").css("height", chat_height + "px");
-    $("#chat-messages").css("height", chat_messages_height +"px");
-    $("#header").css("width", menu_width + "px");
+  window.uiManager.updateSizes();
 });
+
+var WarleagueGame = function(){
+    var MAP      = { tw: 64, th: 48 },
+      TILE     = 32,
+      METER    = TILE,
+      GRAVITY  = 9.8 * 6, // default (exagerated) gravity
+      MAXDX    = 15,      // default max horizontal speed (15 tiles per second)
+      MAXDY    = 60,      // default max vertical speed   (60 tiles per second)
+      ACCEL    = 1/2,     // default take 1/2 second to reach maxdx (horizontal acceleration)
+      FRICTION = 1/6,     // default take 1/6 second to stop from maxdx (horizontal friction)
+      IMPULSE  = 1500,    // default player jump impulse
+      COLOR    = { BLACK: '#000000', YELLOW: '#ECD078', BRICK: '#D95B43', PINK: '#C02942', PURPLE: '#542437', GREY: '#333', SLATE: '#53777A', GOLD: 'gold' },
+      COLORS   = [ COLOR.YELLOW, COLOR.BRICK, COLOR.PINK, COLOR.PURPLE, COLOR.GREY ],
+      KEY      = { SPACE: 32, LEFT: 37, UP: 38, RIGHT: 39, DOWN: 40 };
+      
+  var fps      = 60,
+      step     = 1/fps,
+      canvas   = document.getElementById('game'),
+      ctx      = canvas.getContext('2d'),
+      width    = canvas.width  = MAP.tw * TILE,
+      height   = canvas.height = MAP.th * TILE,
+      player   = {},
+      monsters = [],
+      treasure = [],
+      cells    = [];
+  
+  var t2p      = function(t)     { return t*TILE;                  },
+      p2t      = function(p)     { return Math.floor(p/TILE);      },
+      cell     = function(x,y)   { return tcell(p2t(x),p2t(y));    },
+      tcell    = function(tx,ty) { return cells[tx + (ty*MAP.tw)]; };
+
+  this.render = function() {
+    ctx.clearRect(0, 0, width, height);
+    this.setup(level1);
+    this.renderMap(ctx);
+  }
+
+  this.renderMap = function(ctx) {
+    var x, y, cell;
+    for(y = 0 ; y < MAP.th ; y++) {
+      for(x = 0 ; x < MAP.tw ; x++) {
+        cell = tcell(x, y);
+        if (cell) {
+          ctx.fillStyle = COLORS[cell - 1];
+          ctx.fillRect(x * TILE, y * TILE, TILE, TILE);
+        }
+      }
+    }
+  } 
+
+  this.setup = function(map) {
+    var data    = map.layers[0].data,
+        objects = map.layers[1].objects,
+        n, obj, entity;
+
+    for(n = 0 ; n < objects.length ; n++) {
+      obj = objects[n];
+    }
+
+    cells = data;
+  }
+}
